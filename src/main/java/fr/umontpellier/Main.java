40787ff10125f6ff3318e83249b2bb761c1c1969
tristan-gaido <tristan.gaido01@etu.umontpellier.fr>
2024-11-05 10:13:47 +0100

    correction menu dans le main

--- src/main/java/fr/umontpellier/Main.java
+++ src/main/java/fr/umontpellier/Main.java
@@ -2,7 +2,6 @@ package fr.umontpellier;
 
 import fr.umontpellier.spoon.LogParser;
 import org.apache.logging.log4j.LogManager;
-
 import org.apache.logging.log4j.Logger;
 import spoon.Launcher;
 
@@ -13,18 +12,17 @@ public class Main {
     private static final Logger logger = LogManager.getLogger(Main.class);
 
     public static void main(String[] args) throws IOException {
-
-
+        ProductRepository repository = new ProductRepository();
+//        TestScenarios.main(new String[]{});
 
         LogParser parser = new LogParser();
         parser.parseLogs("./logs/app.log");
         parser.generateProfiles("./profiles");
-        System.out.println("Profiles generated successfully!");//
-//        ProductRepository repository = new ProductRepository();
-//
-//        TestScenarios.main(new String[]{});
-//
-//        Menu menu = new Menu(repository);
-//        menu.start();
+        System.out.println("Profiles generated successfully!");
+
+        Menu menu = new Menu(repository);
+        User interactiveUser = new User(0, "Interactive_User", 30, "interactive@test.com", "pass123");
+        menu.setCurrentUser(interactiveUser);
+        menu.start();
     }
 }
